2bcb88b8998bc74a76ece1c12491f9bdce01c60b
bitcamp <bitcamp@BIT>
2024-08-07 19:29:31 +0900

    08.08.수 제출과제 3번 파일추가

---
+++ Week3/d240808_Thu_Task/e/io/ex06/Prob.java
@@ -0,0 +1,88 @@
+package d240808_Thu_Task.e.io.ex06;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+
+// W3 D14 Task3-2
+
+public class Prob {
+
+	// Field
+
+	// Constructor
+
+	// Method
+	public void printScore(Score score) {
+		System.out.println(score.getName()+" : "
+				+score.getKor()+" "+score.getEng()+" "
+				+score.getMath()+" "+score.getSum());
+	}
+	
+	public Vector<Score> getScore(String fileName) {
+		
+		Vector<Score> scores = new Vector<Score>();
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(fileName));
+			String line = "";
+			
+			while ((line=br.readLine())!=null) {
+				String[] info = line.split("/");
+				
+				for (int i = 0; i < info.length; i++) {
+					info[i] = info[i].trim();
+				}
+				
+				String name = info[0];
+				int kor = Integer.parseInt(info[1]);
+				int eng = Integer.parseInt(info[2]);
+				int math = Integer.parseInt(info[3]);
+				int sum = kor+eng+math;
+			
+				if (info.length!=4) {
+					throw new Exception("과목수가 맞지 않습니다.");
+				}else {
+					scores.add(new Score(name, kor, eng, math, sum));
+				}
+			}
+			
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			
+		} catch (IOException e2) {
+			e2.printStackTrace();
+			
+		} catch (Exception e3) {
+			e3.printStackTrace();
+			System.out.println(e3.getMessage());
+			
+		} finally {
+			try {
+				br.close();
+			} catch (Exception e4) {
+				e4.printStackTrace();
+			}
+		}
+		
+		return scores;
+	}
+	
+	// Main Method
+	public static void main(String[] args) throws Exception {
+		
+		String path = "./Week3/d240808_Thu_Task/e/io/ex06/";
+		Vector<Score> scores = new Prob().getScore(path+"data.txt");
+		
+		for (Score score: scores) {
+			score.printScore();
+		}
+		
+
+	}// main end
+
+}
+// class end
---
+++ Week3/d240808_Thu_Task/e/io/ex06/Score.java
@@ -0,0 +1,78 @@
+package d240808_Thu_Task.e.io.ex06;
+
+import java.util.Vector;
+
+// W3 D14 Task3-1
+
+public class Score {
+
+	// Field
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private int sum;
+	
+	
+	// Constructor
+	public Score(String name, int kor, int eng, int math, int sum) {
+		super();
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.sum = sum;
+	}
+	
+	
+	// Method
+	public void printScore() {
+		System.out.println(this.getName()+" : "
+				+this.getKor()+" "+this.getEng()+" "
+				+this.getMath()+" "+this.getSum());
+	}
+
+	
+	// Getter, Setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+	
+}
+// class end
