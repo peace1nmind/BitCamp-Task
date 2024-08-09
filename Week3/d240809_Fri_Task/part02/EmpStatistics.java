1e44608be654330feba92450b6a2ae9ebac278f3
bitcamp <bitcamp@BIT>
2024-08-09 10:57:17 +0900

    08.09.금 제출과제 02번 java 파일추가

---
+++ Week3/d240809_Fri_Task/part02/EmpStatistics.java
@@ -0,0 +1,95 @@
+package d240809_Fri_Task.part02;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// W D 
+
+public class EmpStatistics {
+
+	// Field
+	private String driverName = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	private String user = "hr";
+	private String password = "hr";
+	
+	// Constructor
+	public EmpStatistics() {
+	}
+
+	// Method
+	public void printSatistics(int maxSalary) throws SQLException {
+		
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName(driverName);
+			
+			conn = DriverManager.getConnection(url, user, password);
+			
+			if (conn != null) {
+				System.out.println("DB conn Success");
+			}
+			
+			String sql = "SELECT j.job_title job_title, AVG(NVL(e.salary, 0)) avgSalary "
+					+ "FROM employees e, jobs j "
+					+ "WHERE e.job_id = j.job_id "
+					+ "AND e.salary >= "+maxSalary+" "
+					+ "GROUP BY j.job_title "
+					+ "ORDER BY avgSalary DESC" ;
+			
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			System.out.println("==============================");
+			System.out.println("Max Salary : "+maxSalary);
+			System.out.println("==============================");
+			
+			while (rs.next()) {
+				String job_title = rs.getString("job_title");
+				int avgSalary = rs.getInt("avgSalary");
+				
+				System.out.println("["+job_title+"] "+avgSalary);
+			}
+			
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+			System.out.println("Class Not Found");
+			System.out.println("DB conn Fail");
+			
+		} catch (SQLException e2) {
+			e2.printStackTrace();
+			System.out.print("SQL Exception");
+			
+		} finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+				
+				if (conn != null) {
+					conn.close();
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}// method end
+	
+	
+	// Main Method
+	public static void main(String[] args) throws SQLException {
+
+		new EmpStatistics().printSatistics(10000);
+
+	}// main end
+
+}
+// class end
